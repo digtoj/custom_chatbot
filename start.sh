8e69fc5d80d377e6c3bee408705e9f8ebc7b4ff4
oscar junior tsakam <88436529+digtoj@users.noreply.github.com>
2024-03-12 10:53:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-streamlit run embedding_app.py --server.port 8501 &
+streamlit run embedding_app.py --server.port 8501 &&
 
-streamlit run openai_based_chat.py --server.port 8502 &
+streamlit run openai_based_chat.py --server.port 8502 &&
 
-streamlit run huggingface_based_chat.py --server.port 8503 &
+streamlit run huggingface_based_chat.py --server.port 8503 &&
 
 streamlit run main_app.py --server.port 80

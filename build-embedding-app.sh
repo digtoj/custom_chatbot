8276e22f30b3dd7251e8f6b0d954dfb84b48b195
oscar junior tsakam <88436529+digtoj@users.noreply.github.com>
2024-03-13 07:42:00 +0100

    rename frontend docker file and build script

--- build-embedding-app.sh
+++ build-embedding-app.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build . -t embedding-app-image:latest -f Dockerfile.embeddingapp
+docker build . -t embedding-backend:latest -f Dockerfile-backend
+
+docker build . -t embedding-app-image:latest -f Dockerfile-embeddingapp

011cae7dcab1c7060362450486ddf5fcd10ad4cb
oscar junior tsakam <88436529+digtoj@users.noreply.github.com>
2024-03-13 08:23:16 +0100

    Update embedding_backend.py

--- embedding_backend.py
+++ embedding_backend.py
@@ -51,4 +51,4 @@ def handle_create_embeddings():
         return jsonify({"error": "Error creating embedding"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)

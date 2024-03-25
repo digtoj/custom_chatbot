08915570e5530ee08dcdbdb88779a4059a579b46
oscar junior tsakam <88436529+digtoj@users.noreply.github.com>
2024-03-25 08:53:20 +0100

    update build script and remove compose chatbot

--- build-chat-app.sh
+++ build-chat-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build . -t chatbot-app-image:latest -f Dockerfile-chatapp
 
-docker compose -f docker-compose-chatbot.yml up
+docker compose up
--- build-embeddingapp.sh
+++ build-embeddingapp.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 docker build . -t embedding-app-image:latest -f Dockerfile-embeddingapp
 
-docker compose up
 

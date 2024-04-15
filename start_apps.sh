82f06074f5a6b9502e89eaf1db4ccfb388802edd
oscar junior tsakam <88436529+digtoj@users.noreply.github.com>
2024-04-15 08:10:40 +0200

    Update start_apps.sh

--- start_apps.sh
+++ start_apps.sh
@@ -3,4 +3,4 @@
 #Start app
 python init_data_extraction.py &
 #streamlit run embedding_app.py --server.port=8501 &
-streamlit run chat_app.py --server.port=8502
+streamlit run streamlit_app.py --server.port=8502

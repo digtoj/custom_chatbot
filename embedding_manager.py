cefb65ed2876f4549c73359a77e641bf453cdf7a
oscar junior tsakam <88436529+digtoj@users.noreply.github.com>
2024-04-15 13:35:00 +0200

    update start script

--- embedding_manager.py
+++ embedding_manager.py
@@ -133,7 +133,7 @@ def create_embedding_from_pdf_file(pdf_directory, embeddings, persist_directory)
 
             data = loader.load()
              # Split  data up into smaller documents with Chunks
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
 
             documents = text_splitter.split_documents(data)
 
